a1dd44e475d73edf2734a7a8e1cd53e60d7da511
alimiza <fajri.hiza@gmail.com>
2026-09-04 08:58:19 +0000

    initial commit

---
+++ smartapp/smart_erp_lhq/doctype/smartapp_dashboard_setting/smartapp_dashboard_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, fajri hiza and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SmartApp Dashboard Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
